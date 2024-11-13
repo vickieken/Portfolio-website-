a46c86350ffb1c677294714b6c87d8b4dcaa3c2a
Kenvickie <vickiekea8@gmail.com>
2024-11-13 07:21:30 +0100

    added about page

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const cancelBar = document.querySelector(".fa-times");
 
 const navOption = document.querySelector(".navbar");
 
+const intro = document.querySelector(".intro");
+
+const navLink = document.querySelectorAll(".nav-link");
+
+// const fillWeb = document.querySelectorAll("");
+
 menuBar.addEventListener("click", ()=> {
     navOption.classList.toggle("showbar");
 })
 
 cancelBar.addEventListener("click", ()=> {
     navOption.classList.toggle("showbar");
-})
+});
+
+navLink.addEventListener("click", ()=> {
+    navOption.classList.toggle("showbar");
+});
